5eaf713ae84756d2e07fe985acc81ae6a8c43d3f
NasibNia <nasibnia@gmail.com>
2018-09-27 23:47:53 -0700

    deploy2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var logger = require ("morgan");
 var mongoose = require ("mongoose");
 
 
-var PORT =  3000;
+var PORT =  PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -20,7 +20,7 @@ app.use(express.static("public"));
 
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/hackerNewsdb";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
