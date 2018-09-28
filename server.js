21ff88cc317a22edff0e68c4daac45b93f039ae7
NasibNia <nasibnia@gmail.com>
2018-09-27 23:37:18 -0700

    ready to push to heroku

--- server.js
+++ server.js
@@ -17,6 +17,17 @@ app.use(logger("dev"));
 
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
+
+
+// // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/hackerNewsdb";
+
+// // Set mongoose to leverage built in JavaScript ES6 Promises
+// // Connect to the Mongo DB
+// mongoose.Promise = Promise;
+// mongoose.connect(MONGODB_URI);
+
+
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/hackerNewsdb", { useNewUrlParser: true });
 // Use body-parser for handling form submissions
